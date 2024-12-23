0533b814bd29cb2e2fe764c6b3bd4ff171d89e3d
user <gen8108@mail.ru>
2024-12-23 19:55:16 +0700

    Создано окно и виджеты

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,21 @@
 from tkinter import *
+from PIL import Image,ImageTk
+import requests
+from io import BytesIO # позволяет работать с байтами, картинка прилетает в виде байтов
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+url = "https://cataas.com/cat" # url адрес в интернете
+img = load_image(url) # функция загрузки изображения в который будем передавать url и будет возвращаться картинка
+#делаем проверку
+
+if img:
+    label.config(image=img) # установим картинку на метку
+    label.image = img # нужно чтобы сборщик мусора не убрал картинку
+
+window.mainloop()
